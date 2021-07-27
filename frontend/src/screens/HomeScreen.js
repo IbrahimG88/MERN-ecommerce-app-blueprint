d67b3ceffa217e440367e1d55067f283c6b9fee0
IbrahimG88 <goharyibrahim@gmail.com>
2021-07-27 18:09:19 +0200

    12. Fetching Products From React (useEffect)

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-import products from "../products";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  /*
+  useState:
+  const [products, setProducts] 
+  products is ewhat we want to call it this piece of state,
+  and setProducts is the function that we will use to manipulate or change this piece of state, products
+ 
+  useState([])
+  the empty array is the default for products value
+  */
+
+  /*
+  useEffect()
+  similar to componentDidMount()
+  */
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+
+      setProducts(data);
+      // set products value to the vaslue of data received in the response
+    };
+
+    fetchProducts();
+  }, []);
+  // the empty array above includes dependencies that fire
+  // useEffect if they change, eg: [test]) if test value changes, fire off useEffect
+
   return (
     <>
       <h1>Latest Products</h1>
