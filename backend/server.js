df7862de5786cde092dc665acf4a365fcd976dd9
IbrahimG88 <goharyibrahim@gmail.com>
2021-08-10 02:52:16 +0500

    error handler middlew. and first redux setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import connectDB from "./config/db.js";
 
 import productRoutes from "./routes/productRoutes.js";
 
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
 
 connectDB();
@@ -16,6 +18,27 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+/*
+// middleware example:
+app.use((err, req, res, next) => {
+  // look the status code if its not 200 then return 500 with includes several errors, else send the status code
+  const error = res.statusCode === 200 ? 500 : res.statusCode;
+  ...
+  the rest errorMiddlware.js file
+});
+*/
+
+// if this explanation and code not clear check video 24 in the course Q and A
+// these are middleware that's code gets executed with this requests above only not work
+// imported from errorMiddleware.js file
+// the middleware notFound will run only as its position in the compiling
+// so it will give notFound error if this app.use("/api/products", productRoutes); returned not found and error 404
+// not found it will pass the error to next
+app.use(notFound);
+
+// any error will take the form of this custom error handler:
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
